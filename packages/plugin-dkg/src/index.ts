e7be38ef66ae717ca4b41ef9aa77a0112497685d
raghav-rama <hackerer528@gmail.com>
2025-03-12 21:10:27 +0530

    update imports

--- packages/plugin-dkg/src/index.ts
+++ packages/plugin-dkg/src/index.ts
@@ -2,8 +2,9 @@ import type { Plugin } from "@elizaos/core";
 
 import { dkgInsert } from "./actions/dkgInsert";
 import { generateKaAction } from "./actions/generateKa";
-
+import { judgeLLM } from "./actions/judgeLLM";
 import { graphSearch } from "./providers/graphSearch";
+import { HypothesisClient } from "./clients";
 
 export * as actions from "./actions";
 export * as providers from "./providers";
@@ -12,6 +13,8 @@ export const dkgPlugin: Plugin = {
     name: "dkg",
     description:
         "Agent DKG which allows you to store memories on the OriginTrail Decentralized Knowledge Graph",
-    actions: [dkgInsert, generateKaAction],
+    actions: [dkgInsert, generateKaAction, judgeLLM],
     providers: [graphSearch],
+    evaluators: [],
+    clients: [HypothesisClient],
 };
