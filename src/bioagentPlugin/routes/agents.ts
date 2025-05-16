9a5f8de8b1579a498a36375199bfbf67eef27d3b
raghav-rama <hackerer528@gmail.com>
2025-05-16 22:32:44 +0530

    feat(routes): add getAgents route

--- src/bioagentPlugin/routes/agents.ts
+++ src/bioagentPlugin/routes/agents.ts
@@ -2,11 +2,22 @@ import { IAgentRuntime } from "@elizaos/core";
 import { sql } from "drizzle-orm";
 import { type Route } from "@elizaos/core";
 
-const query = sql`SELECT COUNT(*) AS total_agents FROM agents`;
-
 async function getTotalAgents(runtime: IAgentRuntime): Promise<BigInt> {
+  const query = sql`SELECT COUNT(*) AS total_agents FROM agents`;
   const totalAgents = await runtime.db.execute(query);
   return totalAgents.rows[0].total_agents as BigInt;
+}
+
+type TAgent = {
+  id: string;
+  name: string;
+  system: string;
+};
+
+async function getAgents(runtime: IAgentRuntime): Promise<TAgent[]> {
+  const query = sql`SELECT id, name, system FROM agents`;
+  const agents = await runtime.db.execute(query);
+  return agents;
 }
 
 export const totalAgents: Route = {
@@ -16,6 +27,17 @@ export const totalAgents: Route = {
     const totalAgents = await getTotalAgents(runtime);
     res.json({
       result: totalAgents,
+    });
+  },
+};
+
+const agents: Route = {
+  path: "/getAgents",
+  type: "GET",
+  handler: async (_req: any, res: any, runtime: IAgentRuntime) => {
+    const agents = await getAgents(runtime);
+    res.json({
+      result: agents,
     });
   },
 };
